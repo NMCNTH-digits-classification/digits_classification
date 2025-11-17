0f3bb3e0ffe9fd4ef8c3aec69a737118304639aa
Halobeat <avungoctuan556@gmail.com>
2025-11-17 22:55:42 +0700

    collate_fn now has a collator(From DataCollator() )

--- trainer.py
+++ trainer.py
@@ -1,11 +1,14 @@
 import torch
 from torch.utils.data import DataLoader, random_split, ConcatDataset
-from torchvision import datasets, transforms
+from torchvision import transforms
 from src.data.dataloader import MnistDataset, DataCollator
 from omegaconf import  OmegaConf
 transform = transforms.ToTensor()
 config = OmegaConf.load("./configs/config.yaml")
-full_dataset= MnistDataset(config.data.root_dir, True, transform)
+full_train_dataset= MnistDataset(config.data.root_dir, True, transform)
+full_test_dataset= MnistDataset(config.data.root_dir, False, transform)
+collator = DataCollator()
+full_dataset = ConcatDataset([full_train_dataset, full_test_dataset])
 
 total_size = len(full_dataset)
 train_size = int(0.6 * total_size)
@@ -18,27 +21,26 @@ train_dataset, val_dataset, test_dataset = random_split(
         generator=torch.Generator().manual_seed(42)
     )
 
-val_data = val_dataset
 train_loader = DataLoader(
     dataset=train_dataset,
     batch_size=config.data.batch_size,
     shuffle=True,
     num_workers=4,
-    collate_fn=None
+    collate_fn=collator
 )
 test_loader = DataLoader(
     dataset=test_dataset,
     batch_size=config.data.batch_size,
     shuffle=False,
     num_workers=4,
-    collate_fn=None
+    collate_fn=collator
 )
 val_loader = DataLoader(
     dataset=val_dataset,
     batch_size=config.data.batch_size,
     shuffle=False,
     num_workers=4,
-    collate_fn=None
+    collate_fn=collator
 )
 
 def main():
@@ -47,7 +49,7 @@ def main():
     print(f"Training: {len(train_dataset)}")        
     print(f"Validation: {len(val_dataset)}")    
     print(f"Testing: {len(test_dataset)}")
-
+#
 if __name__ == '__main__':
     main()
     try:
